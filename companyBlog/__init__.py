d42ad5102cf12a8f074f4bed189c1f55bf0feeb4
Md Shadan Altmash <mdshadanaltmash44@gmail.com>
2021-02-08 00:44:12 +0530

    Error Handling

--- companyBlog/__init__.py
+++ companyBlog/__init__.py
@@ -1,3 +1,9 @@
 from flask import  Flask
 
 app=Flask(__name__)
+
+from companyBlog.core.views import core
+from companyBlog.error_pages.handlers import error_pages
+
+app.register_blueprint(core)
+app.register_blueprint(error_pages)
